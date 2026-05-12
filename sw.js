d5aed6313185969e86ed6ff0fe55b9c3b1f66f2d
ilies-ballouk <balloukilies@gmail.com>
2026-05-12 23:48:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Version du cache : incrémente ce numéro à chaque
    mise à jour pour forcer le rechargement chez tous.
    ═══════════════════════════════════════════════ */
-const CACHE_NAME = 'floppy-wings-v31';
+const CACHE_NAME = 'floppy-wings-v32';
 
 const ASSETS_TO_CACHE = [
   './',
